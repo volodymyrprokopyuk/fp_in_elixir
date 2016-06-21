89b17237baea9f5fde42cce37eda373e629233d7
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-06-21 17:45:15 +0200

    add reduce test for Player module

--- test/01_what_is_fp_test.exs
+++ test/01_what_is_fp_test.exs
@@ -12,4 +12,11 @@ defmodule PlayerTest do
     p2 = %Player{name: "Volodymyr", score: 10}
     assert Player.declare_winner(p1, p2) == "#{p1.name} is the winner!"
   end
+
+  test "reduce over list of palyers with get_winner returns absolute winner" do
+    p1 = %Player{name: "Svitlana", score: 14}
+    p2 = %Player{name: "Volodymyr", score: 10}
+    p3 = %Player{name: "Igor", score: 15}
+    assert Enum.reduce([p1, p2, p3], &Player.get_winner/2) == p3
+  end
 end
