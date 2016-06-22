84bd76c758b0b797881403106111e1c6a7a24fd7
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-06-22 13:44:10 +0200

    add Factorial and Fibonacci modules

--- lib/02_getting_started.ex
+++ lib/02_getting_started.ex
@@ -5,3 +5,21 @@ defmodule Abs do
 
   def format_abs(x), do: "The absolute value of #{x} is #{Abs.abs(x)}"
 end
+
+defmodule Factorial do
+  @moduledoc false
+
+  def factorial(x), do: do_factorial(x, 1)
+  defp do_factorial(0, fac), do: fac
+  defp do_factorial(x, fac), do: do_factorial(x - 1, fac * x)
+end
+
+defmodule Fibonacci do
+  @moduledoc false
+
+  def fibonacci(x), do: do_fibonacci(x, 0, 1)
+  defp do_fibonacci(0, prev, _curr), do: prev
+  defp do_fibonacci(1, _prev, curr), do: curr
+  defp do_fibonacci(x, prev, curr),
+    do: do_fibonacci(x - 1, curr, prev + curr)
+end
--- test/02_getting_started_test.exs
+++ test/02_getting_started_test.exs
@@ -20,3 +20,33 @@ defmodule AbsTest do
     end
   end
 end
+
+defmodule FactorialTest do
+  use ExUnit.Case
+  @moduletag :ch_02
+
+  describe "Factorial.factorial/1" do
+    test "returns factorial of positive numbers" do
+      assert Factorial.factorial(0) == 1
+      assert Factorial.factorial(1) == 1
+      assert Factorial.factorial(5) == 120
+    end
+  end
+end
+
+defmodule FibonacciTest do
+  use ExUnit.Case
+  @moduledoc :ch_02
+
+  describe "Fibonacci.fibonacci/1" do
+    test "returns nth Fibonacci number" do
+      assert Fibonacci.fibonacci(0) == 0
+      assert Fibonacci.fibonacci(1) == 1
+      assert Fibonacci.fibonacci(2) == 1
+      assert Fibonacci.fibonacci(3) == 2
+      assert Fibonacci.fibonacci(4) == 3
+      assert Fibonacci.fibonacci(5) == 5
+      assert Fibonacci.fibonacci(6) == 8
+    end
+  end
+end
