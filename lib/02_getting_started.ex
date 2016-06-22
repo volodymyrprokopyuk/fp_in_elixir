6fe338a9e09d86e901820b958d153aacaaa6936f
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-06-22 17:25:34 +0200

    add BinarySearch module

--- lib/02_getting_started.ex
+++ lib/02_getting_started.ex
@@ -12,6 +12,8 @@ defmodule Factorial do
   def factorial(x), do: do_factorial(x, 1)
   defp do_factorial(0, fac), do: fac
   defp do_factorial(x, fac), do: do_factorial(x - 1, fac * x)
+
+  def format_result(m, x, f), do: "#{m} of #{x} is #{f.(x)}"
 end
 
 defmodule Fibonacci do
@@ -22,4 +24,22 @@ defmodule Fibonacci do
   defp do_fibonacci(1, _prev, curr), do: curr
   defp do_fibonacci(x, prev, curr),
     do: do_fibonacci(x - 1, curr, prev + curr)
+end
+
+defmodule BinarySearch do
+  @moduledoc false
+
+  def find(l, x), do: do_find(Enum.with_index(l), x)
+  defp do_find(l, x) do
+    case Enum.split(l, l |> length |> div(2)) do
+      {[], []} -> -1
+      {_left, [{v, i}]} -> if v == x do i else -1 end
+      {left, [{v, i} | right]} ->
+      cond do
+        v == x -> i
+        v < x -> do_find(right, x)
+        true -> do_find(left, x)
+      end
+    end
+  end
 end
--- test/02_getting_started_test.exs
+++ test/02_getting_started_test.exs
@@ -29,7 +29,27 @@ defmodule FactorialTest do
     test "returns factorial of positive numbers" do
       assert Factorial.factorial(0) == 1
       assert Factorial.factorial(1) == 1
+      assert Factorial.factorial(2) == 2
+      assert Factorial.factorial(3) == 6
+      assert Factorial.factorial(4) == 24
       assert Factorial.factorial(5) == 120
+    end
+  end
+
+  describe "Factorial.format_result/3" do
+    test "returns absolute value for Abs.abs/1" do
+      assert Factorial.format_result("The absolute value", -4,
+        &Abs.abs/1) == "The absolute value of -4 is 4"
+    end
+
+    test "returns factorial for Factorial.factorial" do
+      assert Factorial.format_result("The factorial", 4,
+        &Factorial.factorial/1) == "The factorial of 4 is 24"
+    end
+
+    test "returns the incremented value for anonymous function" do
+      assert Factorial.format_result("The increment", 1,
+        &(&1 + 1)) == "The increment of 1 is 2"
     end
   end
 end
@@ -47,6 +67,25 @@ defmodule FibonacciTest do
       assert Fibonacci.fibonacci(4) == 3
       assert Fibonacci.fibonacci(5) == 5
       assert Fibonacci.fibonacci(6) == 8
+    end
+  end
+end
+
+defmodule BinarySearchTest do
+  use ExUnit.Case
+  @moduletag :ch_02
+
+  describe "BinarySearch.find/2" do
+    test "returns found element index or -1" do
+      assert BinarySearch.find([], 10) == -1
+      assert BinarySearch.find([1], 10) == -1
+      assert BinarySearch.find([1,2], 10) == -1
+      assert BinarySearch.find([1,2,3], 10) == -1
+      assert BinarySearch.find([1], 1) == 0
+      assert BinarySearch.find([1,2], 2) == 1
+      assert BinarySearch.find([1,2,3], 3) == 2
+      assert BinarySearch.find([1,2,3,4], 2) == 1
+      assert BinarySearch.find([1,2,3,4], 3) == 2
     end
   end
 end
