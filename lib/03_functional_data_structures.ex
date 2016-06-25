e1eb61d483a4b85349459ddf289567cc28b29a7f
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-06-25 17:39:04 +0200

    add L.drop

--- lib/03_functional_data_structures.ex
+++ lib/03_functional_data_structures.ex
@@ -6,11 +6,11 @@ defmodule L do
   def empty, do: %L{}
   def cons(head, tail), do: %L{head: head, tail: tail}
 
-  def new(l) when is_list(l), do: do_new(l, empty)
+  def new(l) when is_list(l), do: l |> Enum.reverse |> do_new(L.empty)
   defp do_new([], acc), do: acc
-  defp do_new([h | t], acc), do: do_new(t, cons(h, acc))
+  defp do_new([h | t], acc), do: do_new(t, L.cons(h, acc))
 
-  def to_list(l), do: do_to_list(l, [])
+  def to_list(l), do: l |> do_to_list([]) |> Enum.reverse
   defp do_to_list(%L{tail: :empty}, acc), do: acc
   defp do_to_list(%L{head: h, tail: t}, acc), do: do_to_list(t, [h | acc])
 
@@ -20,5 +20,13 @@ defmodule L do
 
   def product(l), do: do_product(l, 1)
   defp do_product(%L{tail: :empty}, acc), do: acc
+  defp do_product(%L{head: 0}, _acc), do: 0
   defp do_product(%L{head: h, tail: t}, acc), do: do_product(t, acc * h)
+
+  def tail(%L{tail: :empty}), do: raise ArgumentError, "L.tail: empty list"
+  def tail(%L{head: _, tail: t}), do: t
+
+  def drop(%L{tail: :empty}, _n), do: L.empty
+  def drop(l, 0), do: l
+  def drop(%L{head: _h, tail: t}, n), do: drop(t, n - 1)
 end
--- test/03_functional_data_structures_test.exs
+++ test/03_functional_data_structures_test.exs
@@ -42,4 +42,41 @@ defmodule LTest do
       assert L.product(l) == 24.0
     end
   end
+
+  describe "pattern matching" do
+    test "returns the expression of first match" do
+      res =
+        case L.new([1, 2, 3, 4, 5]) do
+          %L{head: x, tail: %L{head: 2, tail: %L{head: 4, tail: _}}} -> x
+          %L{tail: :empty} -> 42
+          %L{head: x, tail:
+            %L{head: y, tail: %L{head: 3, tail: %L{head: 4, tail: _}}}} -> x + y
+          %L{head: x, tail: %L{head: y}} -> x + y
+          %L{head: h, tail: t} -> h + L.sum(t)
+          _ -> 101
+        end
+      assert res == 3
+    end
+  end
+
+  describe "L.tail/1" do
+    test "returns tail of L" do
+      assert [1] |> L.new |> L.tail == L.empty
+      assert [1, 2] |> L.new |> L.tail |> L.to_list == [2]
+    end
+
+    test "raises ArgumentError on empty L" do
+      assert_raise ArgumentError, fn -> [] |> L.new |> L.tail end
+    end
+  end
+
+  describe "L.drop/2" do
+    test "returns L with n elements dropped or empty L" do
+      assert [1, 2] |> L.new |> L.drop(0) |> L.to_list == [1, 2]
+      assert L.empty |> L.drop(1) == L.empty
+      assert [1] |> L.new |> L.drop(1) == L.empty
+      assert [1, 2, 3] |> L.new |> L.drop(1) |> L.to_list == [2, 3]
+      assert [1, 2, 3, 4] |> L.new |> L.drop(2) |> L.to_list == [3, 4]
+    end
+  end
 end
