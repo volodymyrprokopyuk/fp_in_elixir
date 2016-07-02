4b54e691d3d052dffb52aa82f41542c7b439106c
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-07-02 18:10:18 +0200

    add Stat.variance

--- lib/04_functional_error_handling.ex
+++ lib/04_functional_error_handling.ex
@@ -1,6 +1,34 @@
+defmodule Option do
+  @moduledoc false
+
+  defstruct ok: nil, error: :empty
+
+  # data constructors
+  def ok(x), do: %Option{ok: x}
+  def error(e), do: %Option{error: e}
+
+  def get_or(%Option{ok: x, error: :empty}, _d), do: x
+  def get_or(%Option{}, d), do: d
+
+  def or_option(%Option{error: :empty} = o, _d), do: o
+  def or_option(%Option{}, %Option{} = d), do: d
+
+  def map(%Option{ok: x, error: :empty}, f), do: Option.ok(f.(x))
+  def map(%Option{} = o, _f), do: o
+
+  def flat_map(%Option{ok: x, error: :empty}, f), do: f.(x)
+  def flat_map(%Option{} = o, _f), do: o
+end
+
 defmodule Stat do
   @moduledoc false
 
-  def mean([]), do: {:error, "Stat.mean: empty list"}
-  def mean(l), do: {:ok, Enum.sum(l) / length(l)}
+  def mean([]), do: Option.error("Stat.mean: empty list")
+  def mean(l), do: Option.ok(Enum.sum(l) / length(l))
+
+  def variance(l) do
+    with %Option{ok: m, error: :empty} <- Stat.mean(l) do
+      l |> Enum.map(&(:math.pow(&1 - m, 2))) |> Stat.mean
+    end
+  end
 end
--- test/04_functional_error_handling_test.exs
+++ test/04_functional_error_handling_test.exs
@@ -1,12 +1,45 @@
+defmodule OptionTest do
+  use ExUnit.Case
+  @moduletag :ch_04
+
+  describe "Option.map/2" do
+    test "maps over Option or returns error as is" do
+      assert Option.error("oh") |> Option.map(&String.upcase/1) ==
+        Option.error("oh")
+      assert Option.ok("ok") |> Option.map(&String.upcase/1)
+      |> Option.get_or(:error) == "OK"
+    end
+  end
+
+  describe "Option.flat_map/2" do
+    test "flat maps over Option or returns flattened error" do
+      ok = fn x -> Option.ok(x * 10) end
+      err = fn _ -> Option.error("err") end
+      assert Option.error("oh") |> Option.flat_map(ok) == Option.error("oh")
+      assert Option.error("oh") |> Option.flat_map(err) == Option.error("oh")
+      assert Option.ok(1) |> Option.flat_map(ok) == Option.ok(10)
+      assert Option.ok(1) |> Option.flat_map(err) == Option.error("err")
+    end
+  end
+end
+
 defmodule StatTest do
   use ExUnit.Case
   @moduletag :ch_04
 
   describe "Stat.mean/1" do
     test "returns mean of list or error on empty list" do
-      assert [] |> Stat.mean == {:error, "Stat.mean: empty list"}
-      assert [1] |> Stat.mean == {:ok, 1.0}
-      assert [1, 2] |> Stat.mean == {:ok, 1.5}
+      assert [] |> Stat.mean == Option.error("Stat.mean: empty list")
+      assert [1] |> Stat.mean == Option.ok(1.0)
+      assert [1, 2] |> Stat.mean == Option.ok(1.5)
+    end
+  end
+
+  describe "Stata.variance/1" do
+    test "return variance of list or error on empty list" do
+      assert [] |> Stat.variance == Option.error("Stat.mean: empty list")
+      assert [1] |> Stat.variance == Option.ok(0.0)
+      assert [1, 2, 3, 4] |> Stat.variance == Option.ok(1.25)
     end
   end
 end
