c0e44fddc18773bee64fb5de0ac6519c735223a9
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-07-15 15:42:06 +0200

    add Lazy.map and Lazy.constant

--- lib/05_laziness.ex
+++ lib/05_laziness.ex
@@ -1,6 +1,11 @@
 defmodule Lazy do
   @moduledoc false
 
-  def fibonacci, do: Stream.unfold([0, 1],
-        fn [prev, curr] -> {prev, [curr, prev + curr]} end)
+  def map(l, f), do:
+    Stream.unfold(l, fn [] -> nil; [h | t] -> {f.(h), t} end)
+
+  def fibonacci, do: Stream.unfold({0, 1},
+    fn {prev, curr} -> {prev, {curr, prev + curr}} end)
+
+  def constant(x), do: Stream.unfold(x, fn x -> {x, x} end)
 end
--- test/05_laziness_test.exs
+++ test/05_laziness_test.exs
@@ -2,10 +2,24 @@ defmodule LazyTest do
   use ExUnit.Case
   @moduletag :ch_05
 
-  describe "fibonacci/0" do
-    test "returns Fibonacci sequence stream" do
+  describe "Lazy.map/2" do
+    test "maps lazily over list with a function that returns value" do
+      assert [] |> Lazy.map(&(&1 * 10)) |> Enum.to_list == []
+      assert [1, 2, 3, 4] |> Lazy.map(&(&1 * 10)) |> Enum.to_list ==
+        [10, 20, 30, 40]
+    end
+  end
+
+  describe "Lazy.fibonacci/0" do
+    test "returns lazy Fibonacci sequence stream" do
       assert Lazy.fibonacci |> Enum.take(10) ==
         [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    end
+  end
+
+  describe "Lazy.constant/1" do
+    test "returns infinite lazy stream of constant values" do
+      assert :a |> Lazy.constant |> Enum.take(4) == [:a, :a, :a, :a]
     end
   end
 end
