da67a124ff090221258dbe088a9b295eb87a299a
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2016-07-16 16:41:24 +0200

    add Lazy.take and Lazy.take_while

--- lib/05_laziness.ex
+++ lib/05_laziness.ex
@@ -1,11 +1,42 @@
 defmodule Lazy do
   @moduledoc false
 
-  def map(l, f), do:
-    Stream.unfold(l, fn [] -> nil; [h | t] -> {f.(h), t} end)
+  def new(l), do: l |> Stream.map(&(&1))
 
-  def fibonacci, do: Stream.unfold({0, 1},
-    fn {prev, curr} -> {prev, {curr, prev + curr}} end)
+  def to_list(s), do: s |> Enum.to_list
+
+  def take(s, n) do
+    Stream.unfold({s, n}, fn {s, n} ->
+      case s |> Stream.take(1) |> Lazy.to_list do
+        [] -> nil
+        _ when n == 0 -> nil
+        [x] -> {x, {s |> Stream.drop(1), n - 1}}
+      end
+    end)
+  end
+
+  def take_while(s, p) do
+    Stream.unfold(s, fn s ->
+      case s |> Stream.take(1) |> Lazy.to_list do
+        [] -> nil
+        [x] -> if p.(x) do {x, s |> Stream.drop(1)} else nil end
+      end
+    end)
+  end
+
+  def map(s, f) do
+    Stream.unfold(s, fn s ->
+      case s |> Stream.take(1) |> Lazy.to_list do
+        [] -> nil
+        [x] -> {f.(x), s |> Stream.drop(1)}
+      end
+    end)
+  end
 
   def constant(x), do: Stream.unfold(x, fn x -> {x, x} end)
+
+  def iterate(x, f), do: Stream.unfold(x, fn x -> {x, f.(x)} end)
+
+  def fibonacci, do:
+    Stream.unfold({0, 1}, fn {prev, curr} -> {prev, {curr, prev + curr}} end)
 end
--- test/05_laziness_test.exs
+++ test/05_laziness_test.exs
@@ -2,24 +2,54 @@ defmodule LazyTest do
   use ExUnit.Case
   @moduletag :ch_05
 
-  describe "Lazy.map/2" do
-    test "maps lazily over list with a function that returns value" do
-      assert [] |> Lazy.map(&(&1 * 10)) |> Enum.to_list == []
-      assert [1, 2, 3, 4] |> Lazy.map(&(&1 * 10)) |> Enum.to_list ==
-        [10, 20, 30, 40]
+  describe "Lazy.take/2" do
+    test "takes n first elements form a list" do
+      assert [] |> Lazy.new |> Lazy.take(1) |> Lazy.to_list == []
+      assert [1, 2, 3] |> Lazy.new |> Lazy.take(0) |> Lazy.to_list == []
+      assert [1, 2, 3] |> Lazy.new |> Lazy.take(1) |> Lazy.to_list == [1]
+      assert [1, 2, 3] |> Lazy.new |> Lazy.take(2) |> Lazy.to_list == [1, 2]
+      assert [1, 2, 3] |> Lazy.new |> Lazy.take(3) |> Lazy.to_list == [1, 2, 3]
+      assert [1, 2, 3] |> Lazy.new |> Lazy.take(4) |> Lazy.to_list == [1, 2, 3]
     end
   end
 
-  describe "Lazy.fibonacci/0" do
-    test "returns lazy Fibonacci sequence stream" do
-      assert Lazy.fibonacci |> Enum.take(10) ==
-        [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+  describe "Lazy.take_while/2" do
+    test "takes n first elements form a list based on predicate" do
+      p = fn x -> x < 3 end
+      assert [] |> Lazy.new |> Lazy.take_while(p) |> Lazy.to_list == []
+      assert [1] |> Lazy.new |> Lazy.take_while(p) |> Lazy.to_list == [1]
+      assert [1, 2] |> Lazy.new |> Lazy.take_while(p) |> Lazy.to_list == [1, 2]
+      assert [1, 2, 3] |> Lazy.new |> Lazy.take_while(p) |> Lazy.to_list ==
+        [1, 2]
+      assert [1, 2, 3] |> Lazy.new |> Lazy.take_while(fn _ -> false end)
+      |> Lazy.to_list == []
+    end
+  end
+
+  describe "Lazy.map/2" do
+    test "maps lazily over list with a function that returns value" do
+      f = fn x -> x * 10 end
+      assert [] |> Lazy.new |> Lazy.map(f) |> Lazy.to_list == []
+      assert [1, 2, 3, 4] |> Lazy.map(f) |> Lazy.to_list == [10, 20, 30, 40]
     end
   end
 
   describe "Lazy.constant/1" do
     test "returns infinite lazy stream of constant values" do
       assert :a |> Lazy.constant |> Enum.take(4) == [:a, :a, :a, :a]
+    end
+  end
+
+  describe "Lazy.iterate/s" do
+    test "iterates lazly from initial value applying a function" do
+      f = fn x -> x + 1 end
+      assert 0 |> Lazy.iterate(f) |> Enum.take(4) == [0, 1, 2, 3]
+    end
+  end
+
+  describe "Lazy.fibonacci/0" do
+    test "returns lazy Fibonacci sequence stream" do
+      assert Lazy.fibonacci |> Enum.take(9) == [0, 1, 1, 2, 3, 5, 8, 13, 21]
     end
   end
 end
